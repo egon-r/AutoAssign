e68866f7c4068c4f1202dc19bdb795826d789185
egon-r <egonr@tuta.io>
2023-02-24 15:09:31 +0100

    squash(7): public

---
+++ Scripts/Runtime/AutoAssign.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace ERAutoAssign {
+    public class AutoAssignable : Attribute { }
+
+    public class ManualAssign : Attribute { }
+
+    public class AutoAssign : MonoBehaviour { }
+}
